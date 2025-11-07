603bc52d94261dc0a6c1c3022e289c7c9db81ade
James-Lu-none <jamesabcde277@gmail.com>
2025-11-08 02:41:57 +0800

    remove base and add medium

--- models.py
+++ models.py
@@ -2,7 +2,17 @@ from transformers import WhisperProcessor, WhisperForConditionalGeneration
 from transformers import AutoProcessor, AutoModelForSpeechSeq2Seq
 
 def openai_whisper_small():
+    # 0.2B parameters
     model_name = "openai/whisper-small"
+
+    processor = AutoProcessor.from_pretrained(model_name)
+    model = AutoModelForSpeechSeq2Seq.from_pretrained(model_name)
+    model.config.use_cache = False
+    return processor, model
+
+def openai_whisper_medium():
+    # 0.8B parameters
+    model_name = "openai/whisper-medium"
 
     processor = AutoProcessor.from_pretrained(model_name)
     model = AutoModelForSpeechSeq2Seq.from_pretrained(model_name)
